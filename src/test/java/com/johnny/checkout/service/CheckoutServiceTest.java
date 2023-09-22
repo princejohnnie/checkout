1810402d27830a61f25e491bb286b92cf44b5f26
JohnUzodinma <john.uzodinma@centilytics.com>
2023-09-22 18:57:37 +0100

    Added more Tests

--- src/test/java/com/johnny/checkout/service/CheckoutServiceTest.java
+++ src/test/java/com/johnny/checkout/service/CheckoutServiceTest.java
@@ -36,6 +36,44 @@ public class CheckoutServiceTest {
 
         Assertions.assertThat(checkoutResponse).isNotNull();
         Assertions.assertThat(checkoutResponse.getStatusCode().value()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        Assertions.assertThat(checkoutResponse.getBody().getErrors().get("cardNumber")).isEqualTo("Card number should be between 16 and 19 digits");
 
     }
+
+    @Test
+    public void CheckoutService_CheckCardDetails_ReturnsIncorrectCardNumber() {
+        CheckoutRequest checkoutRequest = new CheckoutRequest("401288888888188112", "12/23", "123");
+
+        ResponseEntity<CheckoutResponse> checkoutResponse = checkoutService.checkCardDetails(checkoutRequest);
+
+        Assertions.assertThat(checkoutResponse).isNotNull();
+        Assertions.assertThat(checkoutResponse.getStatusCode().value()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        Assertions.assertThat(checkoutResponse.getBody().getErrors().get("cardNumber")).isEqualTo("Incorrect Card Number");
+
+    }
+
+    @Test
+    public void CheckoutService_CheckCardDetails_ReturnsInvalidExpiryDate() {
+        CheckoutRequest checkoutRequest = new CheckoutRequest("4012888888881881", "12/22", "123");
+
+        ResponseEntity<CheckoutResponse> checkoutResponse = checkoutService.checkCardDetails(checkoutRequest);
+
+        Assertions.assertThat(checkoutResponse).isNotNull();
+        Assertions.assertThat(checkoutResponse.getStatusCode().value()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        Assertions.assertThat(checkoutResponse.getBody().getErrors().get("expiryDate")).isEqualTo("Invalid Expiry Date");
+
+    }
+
+    @Test
+    public void CheckoutService_CheckCardDetails_ReturnsInvalidCvv() {
+        CheckoutRequest checkoutRequest = new CheckoutRequest("401288888888188", "12/23", "1234");
+
+        ResponseEntity<CheckoutResponse> checkoutResponse = checkoutService.checkCardDetails(checkoutRequest);
+
+        Assertions.assertThat(checkoutResponse).isNotNull();
+        Assertions.assertThat(checkoutResponse.getStatusCode().value()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        Assertions.assertThat(checkoutResponse.getBody().getErrors().get("cvv")).isEqualTo("CVV must be 3 digits");
+
+    }
+
 }
